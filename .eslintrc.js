9c29531039766cddd6d97fadf5a1259086582a8e
Yash Kalaria <yash.kalaria@tntra.io>
2021-08-21 13:21:10 +0530

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['airbnb'],
   rules: {
     'react/jsx-filename-extension': 'off',
-    //You can override any rules you want
+    'react/react-in-jsx-scope': 'off',
+    // You can override any rules you want
   },
 };
--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log("wwwwwwwwwwwwww", ghmnn);
-
-  let a;
-
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
