2956aee7b89362b3858c8de97021b239864e7ae4
Yash Kalaria <yash.kalaria@tntra.io>
2021-08-21 13:42:11 +0530

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-headerr">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
